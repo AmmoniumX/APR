492b6e5df3da81f179ddc761f3d45e8d708e2255
Diego Garcia <diego.garcia.cr@gmail.com>
2026-07-23 13:12:42 -0600

    simplify cmake-setup.sh

--- cmake-setup.sh
+++ cmake-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Configure debug and release presets, then symlink compile_commands.json to project root
-[[ -L compile_commands.json || -f compile_commands.json ]] && rm ./compile_commands.json
+set -e
+rm -f ./compile_commands.json
 cmake --preset debug
 cmake --preset release
 ln -s build/debug/compile_commands.json ./compile_commands.json
